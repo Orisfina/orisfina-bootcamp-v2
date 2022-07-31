de9326c3fe00d08f30b32c3702f7132975455ad3
Joshua Oyewole <joshpp2013@gmail.com>
2022-07-31 07:28:01 +0100

    student Route updated

--- pages/api/students/index.js
+++ pages/api/students/index.js
@@ -14,7 +14,7 @@ export default async function handler(req,res) {
       const newStudent = await user.find({});
       res.status(201).json(newStudent);
     } catch (error) {
-      res.status(500).json(error)
+      res.status(500).json(error.message)
     }
   }
   
