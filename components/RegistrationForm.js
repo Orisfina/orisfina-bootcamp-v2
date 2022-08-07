dd94d53804ad4f8a9a20aee153872f77de48fcc6
Joshua Oyewole  (Orisfina) <46235310+JoshuaOyewole@users.noreply.github.com>
2022-08-07 21:20:55 +0100

    revert registration form to previous state
    The registration page was reverted to the previous codes before the backend integration started

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -1,137 +1,58 @@
-import React,{useState} from 'react'
-import { useRouter } from 'next/router';
-import axios from 'axios';
-
+import React from 'react'
 
 export default function RegistrationForm() {
-
-  const router = useRouter();
-  const [RegistrationError, setRegistrationError] = useState("");
-
-  const [credentials,setCredentials] = useState({
-    address:undefined,
-    firstname: undefined,
-    lastname: undefined,
-    email: undefined,
-    password: undefined,
-    phone: undefined,
-    gender: undefined,
-    laptopStatus:undefined,
-    studentStatus:undefined,
-    course:undefined
-  });
-
-  const handleChange = (e) =>{
-    setCredentials(
-      (prev)=>({
-        ...prev, 
-        [e.target.name]:e.target.value}
-      )
-    )
-  }
-
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
-
-    try{
-      
-      const response = await fetch("http://localhost:5000/api/student/register", {
-        method:"POST",
-        body:JSON.stringify(credentials),
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      });
-      const data = await response.json();
-      console.log(data);
-      if(data.status === 200 && data.success === true){
-        return router.push('/success'); 
-      }
-     setRegistrationError("An Error Occured, Try Again!");
-    }
-    catch(err){
-      console.log(err);
-    }
-  }
-
   return (
     <>
-       <form onSubmit={handleSubmit} name="Registration-Form" autoComplete='off' data-netlify="true" data-netlify-honeypot="bot-field">
+       <form action="/success" method="post" name="Registration-Form" autoComplete='off' data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="form-name" value="Registration-Form" />
                     <div className='form-group'>
-                     <label htmlFor='firstname' className='form-label'> First Name  </label>
-                     <input type='text' id='firstname' name='firstname' onChange={handleChange} className='group-field' data-type="text"  required />
+                     <label htmlFor='fName' className='form-label'> First Name  </label>
+                     <input type='text' id='fName' name='fName' className='group-field' data-type="text"  required />
                    </div>
                    <div className='form-group'>
-                     <label htmlFor='lastname' className='form-label'> Last Name </label>
-                     <input type='text' id='lastname' name='lastname' onChange={handleChange} className='group-field' data-type="text"  required />
+                     <label htmlFor='lName' className='form-label'> Last Name </label>
+                     <input type='text' id='lName' name='last-name' className='group-field' data-type="text"  required />
                    </div>
                    <div className='form-group'>
                      <label className='form-label' htmlFor='email' name='email'> Email </label>
-                     <input type='email' name='email' className='group-field' onChange={handleChange} placeholder="example@email.com" id='email' data-type="text"  required />
-                   </div>
-
-                   <div className='form-group'>
-                     <label className='form-label' htmlFor='password' name='password'> Password </label>
-                     <input type='password' name='password' className='group-field' onChange={handleChange} placeholder="enter your password..." id='password' data-type="text"  required />
+                     <input type='email' name='email' className='group-field' placeholder="example@email.com" id='email' data-type="text"  required />
                    </div>
                    
                    <div className='form-group'>
                      <label className='form-label' htmlFor='phone'> Phone </label>
-                     <input type='number' id='phone' name='phone' className='group-field' data-type="number" onChange={handleChange} maxLength={12} minLength={0}  required />
+                     <input type='tel' name='phone' className='group-field' data-type="text"  required />
                    </div>
-                   <div className='form-group' id='gender' >
-                          <label className='form-label' htmlFor='sex'> Sex: </label>
-                          <label htmlFor="male">
-                          <input 
-                            type="radio" 
-                            name="gender" 
-                            value="Male"
-                            checked={credentials.gender === 'Male'}
-                            onChange={handleChange}
-                          />
-                            Male
-                          </label> 
-                          
-                          <label htmlFor="css" className='ml-s'>
-                            <input 
-                              type="radio" 
-                              name="gender" 
-                              value='Female' 
-                              checked={credentials.gender === 'Female'}
-                              onChange={handleChange}
-                            />  
-                            Female
-                          </label>
-                           
+                   <div className='form-group'>
+                          <label className='form-label' htmlFor='sex'> Sex:</label>
+                          <label htmlFor="male">Male</label> 
+                          <input type="radio" id="male" name="sex" value="Male" />
+                          <label htmlFor="css" className='ml-s'>Female</label>
+                          <input type="radio" id="female" name="sex" value='Female' />   
                    </div>
                    <div className='form-group'>
                         <label className='form-label' htmlFor='courses'>Select Course:</label>
-                          <select name="course" id="course" onChange={handleChange} required>
-                          <option value="null" disabled>Select Course...</option>
-                              <option value="Front-end Development" >Frontend Web Development</option>
-                              <option value="Backend Development">Backend Web Development</option>
-                              <option value="Full-Stack Enginneer">Full-Stack Development</option>
-                              <option value="Graphic Design">Graphics Design</option>
-                              <option value="Application Package">Application Package</option>
-                              <option value="Mobile App Development">Mobile App Development</option>
-                              <option value="Mobile App Development">Hardware Engineering</option>
+                          <select name="courses" id="courses">
+                          <option value="Frontend Web Development">Frontend Web Development</option>
+                          <option value="Backend Web Development">Backend Web Development</option>
+                          <option value="Full-stack Development">Full-Stack Development</option>
+                          <option value="Graphics Design">Graphics Design</option>
+                          <option value="Application Package">Application Package</option>
+                          <option value="Mobile App Development">Mobile App Development</option>
+                          <option value="Mobile App Development">Hardware Engineering</option>
                           </select>
                    </div>
                    <div className='form-group'>
-                        <label className='form-label' htmlFor='studentStatus'>Are you a Student?:</label>
-                          <select name="studentStatus" id="studentStatus" onChange={handleChange} required>
-                          <option  disabled>Select...</option>
+                        <label className='form-label' htmlFor='student-status'>Are you a Student?:</label>
+                          <select name="student-status" id="student-status">
                           <option value="Yes">Yes</option>
                           <option value="No">No</option>
                           </select>
                    </div>
                    <div className='form-group'>
                         <label className='form-label' htmlFor='haveLaptop'>Do you have a Laptop or one you can lend?:</label>
-                          <select name="laptopStatus" id="laptopStatus" onChange={handleChange} required>
-                             <option  disabled>Select...</option>
-                            <option value="Yes">Yes</option>
-                            <option value="No">No</option>
+                          <select name="haveLaptop" id="haveLaptop">
+                          <option value="Yes">Yes</option>
+                          <option value="No">No</option>
                           </select>
                    </div>
                    <div className='form-group'>
