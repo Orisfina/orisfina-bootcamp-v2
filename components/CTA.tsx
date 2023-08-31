acc8cdbf29d66a372f7ebb29c1f7d44c15ea2ba0
joshua Oyewole <joshpp2013@gmail.com>
2023-08-31 18:16:02 +0100

    created a CTA section for easy reuse

---
+++ components/CTA.tsx
@@ -0,0 +1,16 @@
+import Button from './Button'
+
+function CTA() {
+  return (
+    <div className="apply-now">
+    <div className="z-indez-high">
+         <h2>What are you still waiting for?</h2>
+         <p>Application for our 12-weeks bootcamp for all courses starting September 2023 is now open</p>
+         <Button cName='btn blog_btn inline-block' link='/register'>Enroll Now</Button>
+    </div>
+    <div className="bg-overlay"></div>
+</div>
+  )
+}
+
+export default CTA
