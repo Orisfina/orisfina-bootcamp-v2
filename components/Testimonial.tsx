97d6a1f9ac067d767a20cbda5cbdff289c453897
joshua Oyewole <joshpp2013@gmail.com>
2023-08-31 18:19:30 +0100

    corrected some Typos

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 
 const Testimonial = () => {
     return (
-        <>
             <section className="testimonial t-center">
                 <h2 className=" primary__text white-heading">
                     Testimonial
@@ -18,7 +17,7 @@ const Testimonial = () => {
                                 <div className="testimonial4_slide grid1-2">
                                     <Image src={testimonialImg} className="img-circle img-responsive" alt='testisfier' />
                                     <div>
-                                        <p className='t-justify'>Joining Orisfina bootcamp was one of the best decisions i took. Their tutors are the best in town as they are always ready to explain in the most simplest  method. They are just so supportive. In less than 6 weeks i started building basic website. I recommend Orisfina bootcamp to anyone seeking to acquire a skill in web design</p>
+                                        <p className='t-justify'>Joining Orisfina bootcamp was one of the best decisions I took. Their tutors are the best in town as they are always ready to explain in the most simplest  method. They are just so supportive. In less than 6 weeks I started building basic website. I recommend Orisfina bootcamp to anyone seeking to acquire the necessary skill required of a Software Developer</p>
                                         <h4>Betty Yakubu</h4>
                                     </div>
 
@@ -33,7 +32,7 @@ const Testimonial = () => {
                                 <div className="testimonial4_slide grid1-2">
                                     <Image src={testimonialImg2} className="img-circle img-responsive" alt='testisfier' />
                                     <div>
-                                        <p className='t-justify'>No place like Orisfina Bootcamp. From the very first day i registered till date, i have never had a reason to regret my actions. Irrespective of my zero background in coding, I got tutored in such a way that i felt so so easy to grasp. Right now am creating Softwares i never dreamt of few months ago.</p>
+                                        <p className='t-justify'>No place like Orisfina Bootcamp. From the very first day i registered till date, I have never had a reason to regret my actions. Irrespective of my zero background in coding, I got tutored in such a way that i felt so so easy to grasp. Right now am creating Software Applications I never dreamt of few months ago.</p>
                                         <h4>Ani Hope</h4>
                                     </div>
 
@@ -43,15 +42,7 @@ const Testimonial = () => {
                     </div>
                 </div>
             </section>
-            <div className="apply-now">
-                <div className="z-indez-high">
-                    <h2>What are you still waiting for?</h2>
-                    <p>Application for our 12-weeks bootcamp for all courses starting December 2022 is now open</p>
-                    <Button cName='btn blog_btn inline-block' link='/register'>Enroll Now</Button>
-                </div>
-                <div className="bg-overlay"></div>
-            </div>
-        </>
+   
     )
 }
 
